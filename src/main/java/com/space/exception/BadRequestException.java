752ca45c462e1c67b8b0fae2cc92c37674283e65
Sharypov-Denis <sharypov.denis@gmail.com>
2020-05-17 11:45:17 +0300

    Init

---
+++ src/main/java/com/space/exception/BadRequestException.java
@@ -0,0 +1,15 @@
+package com.space.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+/*
+@ResponseStatus(value = HttpStatus.BAD_REQUEST)
+public class BadRequestException extends RuntimeException {
+}
+*/
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class BadRequestException extends RuntimeException{
+    public BadRequestException(String message) {
+        super(message);
+    }
+}
